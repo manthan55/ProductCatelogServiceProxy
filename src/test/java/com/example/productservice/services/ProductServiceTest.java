5c5946e483ca7136fd4dac3cfbe497fc1988090a
Manthan Patidar <manthanpatidar@gmail.com>
2024-03-18 10:49:17 +0530

    ToString in product class

--- src/test/java/com/example/productservice/services/ProductServiceTest.java
+++ src/test/java/com/example/productservice/services/ProductServiceTest.java
@@ -30,5 +30,6 @@ public class ProductServiceTest {
         p1.setId(p1.getId()+1);
         p1.setTitle("Laptop 1");
         productRepository.save(p1);
+        System.out.print(p1.toString());
     }
 }
